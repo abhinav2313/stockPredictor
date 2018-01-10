692770f34bf8c2e331cc3d6e39302d1c455cf028
Sharma, Abhinav <abhsharm@visa.com>
2018-01-09 21:08:35 -0600

    adding  reading and financial analysis functionality

---
+++ config.py
@@ -0,0 +1,15 @@
+#List of stocks
+stocks = ['AAC','AAME','AAU','AAV','ABAC','ABCD','ABDC','ABEV','ABIL','ABIO','ABR','ABUS','ACERW','ACFC','ACHN',
+                'ACHV','ACMR','ACRX','ACST','ACTG','ADAP','ADES','ADMA','ADMP','ADOM','ADRO','ADVM','ADXS','ADXSW',
+                'AEG','AEHR','AEMD','AETI','AEY','AEZS','AFMD','AG','AGC','AGEN','AGFS','AGFSW','AGI','AGLE','AGRX',
+                'AGTC','AHC','AHPAW','AHPI','AHT','AINV','AIRG','AIRI','AKER','AKG','AKS','AKTS','AKTX','ALDX','ALIM','ALJJ',
+          'ALLT','ALN','ALO','ALQA','ALRN','ALSK','ALT','AMBR','AMCN','AMDA','AMMA','AMPE','AMRC','AMRH','AMRHW','AMRN','AMRS',
+          'AMS','AMSC','AMTX','ANDAR','ANDAW','ANFI','ANH','ANTH','ANW','ANY','AOD','APDN','APDNW','APEN','APHB','APOP',
+          'APOPW','APPS','APRI','APRN','APT','APTO','APVO','APWC','AQB','AQMS','ARAY','ARC','ARCI','ARCT','ARCW','ARDM',
+          'ARDX','AREX','ARGS','ARLZ','ARQL','ARTW','ARTX','ARWR','ASC','ASFI','ASG','ASM','ASNA','ASPN','ASPU','ASRV',
+          'AST','ASTC','ASV','ASX','AT','ATAI','ATAX','ATEC','ATEN','ATHX','ATLC','ATNM','ATOM','ATOS','ATRS','ATTO','ATTU',
+          'ATXI','AU','AUDC','AUG','AUMN','AUO','AUPH','AUTO','AUY','AVAL','AVDL','AVEO','AVGR','AVH','AVID','AVIR','AVP',
+          'AVXL','AWP','AWRE','AWX','AXAS','AXON','AXR','AXSM','AXTI','AXU','AYTU','AZRX','BASI','BBDO','BBG','BBOX','BBRG']
+
+#Configurations for alert
+requiredChangePercentageAmount = 5
